f31026b8665f6c0dc1cd873cbab76e5367d41d52
mryevhenii07 <mrit.works07@gmail.com>
2022-11-18 16:29:43 +0200

    (lessen_5} finished first task

--- task_1.js
+++ task_1.js
@@ -1,20 +1,68 @@
-const concatStrings = (firstString = '', separator = '') => {
-  return (secondString) => {
-    if (typeof separator !== 'string') {
-      separator = '';
+class Stack {
+  constructor(maxSize = 10) {
+    if (
+      typeof maxSize !== 'number' ||
+      maxSize === Infinity ||
+      maxSize === -Infinity ||
+      maxSize === 0
+    ) {
+      throw new Error('Invalid limit value');
     }
 
-    if (typeof secondString !== 'string') {
-      return `${firstString}`;
+    this.maxSize = maxSize;
+    this.stack = {};
+    this.stackCounter = 0;
+  }
+
+  push(elem) {
+    if (this.stackCounter >= this.maxSize) {
+      throw new Error('Limit exceeded');
     }
 
-    return concatStrings(`${firstString}${separator}${secondString}`, separator);
+    this.stackCounter += 1;
+    this.stack[this.stackCounter] = elem;
+  }
+
+  pop() {
+    if (this.stackCounter === 0) {
+      throw new Error('Empty stack');
+    }
+
+    const elem = this.stack[this.stackCounter];
+    delete this.stack[this.stackCounter];
+    this.stackCounter -= 1;
+    return elem;
+  }
+
+  peek() {
+    if (this.stack.length === 0) {
+      return null;
+    }
+
+    return this.stack[this.stackCounter - 1];
+  }
+
+  isEmpty() {
+    return this.stackCounter === 0;
+  }
+
+  toArray() {
+    return Object.values(this.stack);
+  }
+
+  static fromIterable = (iterable) => {
+    if (typeof iterable[Symbol.iterator] !== 'function') {
+      throw new Error('Not iterable');
+    }
+
+    const newStack = new Stack(iterable.length);
+
+    for (const elem of iterable) {
+      newStack.push(elem);
+    }
+
+    return newStack;
   };
-};
+}
 
-concatStrings('first')('second')('third')();
-concatStrings('first', null)('second')()('third');
-concatStrings('first', '123')('second')('third')();
-concatStrings('some-value')('')('')(null);
-concatStrings('some-value')(2);
-concatStrings('some-val')('333')(123n)('end');
+// module.exports = { Stack };
--- task_2.js
+++ task_2.js
@@ -1,56 +1,163 @@
-class Calculator {
-  constructor(firstNumber, secondNumber) {
-    if (arguments.length > 2) {
-      throw new Error();
-    }
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//     this.prev = null;
+//   }
+// }
 
-    this.setX(firstNumber);
-    this.setY(secondNumber);
-  }
+// class LinkedList {
+//   constructor() {
+//     this.top = null;
+//     this.tail = null;
+//     this.size = 0;
+//   }
 
-  isInvalidNumber(number) {
-    if (isNaN(number)) {
-      return true;
-    }
-  }
+//   append(value) {
+//     const node = new Node(value);
 
-  setX(number) {
-    if (this.isInvalidNumber(number)) {
-      throw new Error();
-    }
+//     if (!this.top) {
+//       this.top = node;
+//       this.tail = node;
+//     } else {
+//       this.tail.next = node;
+//       node.prev = this.tail;
+//       this.tail = node;
+//     }
 
-    this.firstNumber = number;
-  }
+//     this.size++;
+//   }
 
-  setY(number) {
-    if (this.isInvalidNumber(number)) {
-      throw new Error();
-    }
+//   prepend(value) {
+//     const node = new Node(value);
 
-    this.secondNumber = number;
-  }
+//     if (!this.top) {
+//       this.top = node;
+//       this.tail = node;
+//     } else {
+//       this.top.prev = node;
+//       node.next = this.top;
+//       this.top = node;
+//     }
 
-  getSum = () => {
-    console.log(this.firstNumber + this.secondNumber);
-  };
+//     this.size++;
+//   }
 
-  getMul = () => {
-    console.log(this.firstNumber * this.secondNumber);
-  };
+//   pop() {
+//     if (!this.top) {
+//       return null;
+//     }
 
-  getSub = () => {
-    console.log(this.firstNumber - this.secondNumber);
-  };
+//     const node = this.tail;
 
-  getDiv = () => {
-    if (this.secondNumber === 0) {
-      throw new Error();
-    }
+//     if (this.size === 1) {
+//       this.top = null;
+//       this.tail = null;
+//     } else {
+//       this.tail = node.prev;
+//       this.tail.next = null;
+//       node.prev = null;
+//     }
 
-    console.log(this.firstNumber / this.secondNumber);
-  };
-}
+//     this.size--;
 
-const calculator = new Calculator(12, 3);
-calculator.getSum();
-calculator.getDiv();
+//     return node.value;
+//   }
+
+//   find(value) {
+//     let current = this.top;
+
+//     while (current) {
+//       if (current.value === value) {
+//         return current;
+//       }
+
+//       current = current.next;
+//     }
+
+//     return null;
+//   }
+
+//   toArray() {
+//     const arr = [];
+//     let current = this.top;
+
+//     while (current) {
+//       arr.push(current.value);
+//       current = current.next;
+//     }
+
+//     return arr;
+//   }
+
+//   static fromIterable(iterable) {
+//     if (iterable[Symbol.iterator] === undefined) {
+//       throw new Error('Not iterable');
+//     }
+
+//     const list = new LinkedList();
+
+//     for (const elem of iterable) {
+//       list.append(elem);
+//     }
+
+//     return list;
+//   }
+// }
+
+// class Stack {
+//   constructor(maxSize) {
+//     if (isNaN(maxSize)) {
+//       maxSize = 10;
+//     }
+
+//     this.size = maxSize;
+//     this.list = new LinkedList();
+//   }
+
+//   push(elem) {
+//     if (this.list.length >= this.size) {
+//       throw new Error('Limit exceeded');
+//     }
+
+//     this.list.append(elem);
+//   }
+
+//   pop() {
+//     if (this.list.length === 0) {
+//       throw new Error('Empty stack');
+//     }
+
+//     return this.list.pop();
+//   }
+
+//   peek() {
+//     if (this.list.length === 0) {
+//       return null;
+//     }
+
+//     return this.list.tail.value;
+//   }
+
+//   isEmpty() {
+//     return this.list.length === 0;
+//   }
+
+//   toArray() {
+//     return this.list.toArray();
+//   }
+
+//   static fromIterable(iterable) {
+//     if (iterable[Symbol.iterator] === undefined) {
+//       throw new Error('Not iterable');
+//     }
+
+//     const stack = new Stack();
+
+//     for (const elem of iterable) {
+//       stack.push(elem);
+//     }
+
+//     return stack;
+//   }
+// }
